ba9b1f3f113a08552235d7a145cade8f89a17caa
Artem <artkrasov@gmail.com>
2022-08-12 22:34:45 +0300

    Started working with the database

---
+++ Notes.Application/Interface/INotesDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Notes.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Interface
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ Notes.Application/Persistence/DbInitializer.cs
@@ -0,0 +1,10 @@
+﻿namespace Notes.Application.Persistence
+{
+    public class DbInitializer
+    {
+        public static void Initialize(NotesDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
